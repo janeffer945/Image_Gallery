a7a41fd5d3328aa1a23eca60a80f1dddb3846fd2
janeffer945 <janeffernjeri945@gmail.com>
2022-06-05 22:35:20 +0300

    initial commit

---
+++ gallery/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path,re_path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+  path('', views.home, name='home'),
+  path('search/', views.search_results, name='search_results'),
+  re_path('location/(?P<location_id>\d+)', views.location , name='location'),
+  re_path('category/(?P<category_id>\d+)', views.category , name='category'),
+  re_path('image/(?P<image_id>\d+)', views.one_image, name='image'),
+
+]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
