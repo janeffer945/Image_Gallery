7e7ccdcea86f014da9781eda4a5b502fde64c2d1
janeffer945 <janeffernjeri945@gmail.com>
2022-06-06 01:45:07 +0300

    added  test save category

--- myapp/tests.py
+++ myapp/tests.py
@@ -67,3 +67,12 @@ class CategoryTestClass(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.category, Category))
+    def test_save_category(self):
+        self.category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_category(self):
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)    
