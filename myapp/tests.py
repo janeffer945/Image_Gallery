193298f29063766ea9a6fc9fce2a202a9da78bd9
janeffer945 <janeffernjeri945@gmail.com>
2022-06-06 01:20:31 +0300

    fixed bugs on test

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
 
+  
+from django.test import TestCase
+from .models import Image,Location,Category
 # Create your tests here.
+class GalleryTest(TestCase):
+    def setUp(self):
+        self.new_category = Category(category_name='food')
+        self.new_category.save_category()
+        self.new_location = Location(location_name = 'kenya')
+        self.new_location.save_location()
+        self.new_image = Image(id=1,img_name='apples', img_decription='this is an apple',img_path='media/pictures/apples-1776744_1920.jpg',img_category=self.new_category,img_location=self.new_location)
+
