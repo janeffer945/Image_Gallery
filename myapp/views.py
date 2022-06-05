6d69dcf899fa4d2f92e58839dc61793822947c2b
janeffer945 <janeffernjeri945@gmail.com>
2022-06-06 01:13:45 +0300

    added haven't searched for any image

--- myapp/views.py
+++ myapp/views.py
@@ -12,3 +12,26 @@ def display(requests):
     page = requests.GET.get('page')
     images_all = p.get_page(page)
     nums = "a" * images_all.paginator.num_pages
+
+
+    title= 'Home'
+
+
+    return render(requests,'index.html',{'all_images':all_images,'locations':locations,'categories':categories,'title':title,'nums':nums,'images_all':images_all})
+
+def search_results(request):
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = 'Search'
+    if 'searchby' in request.GET and request.GET["searchby"]:
+        search_term = request.GET.get("searchby")
+        searched_images=Image.search_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'single.html',{"message":message,"all_images": searched_images,'locations':locations,'categories':categories, 'title':title})
+
+    else:
+        message = "You haven't searched for any image"
+        return render(request, 'index.html',{"message":message, 'locations':locations,'categories':categories, 'title':title})
+
+
