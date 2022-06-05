7bcd58e9e9b52785157aa269ec2244c31187b906
janeffer945 <janeffernjeri945@gmail.com>
2022-06-06 01:56:02 +0300

    registered models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from . models import Image,Location,Category
+
+# Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Location)
+admin.site.register(Category)
