b3c69728c1b8e2624be7620da717cc5efd9b28fc
janeffer945 <janeffernjeri945@gmail.com>
2022-06-06 01:49:05 +0300

    ft:fixed bugs on test

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location_name=models.CharField(max_length=30,unique=True)
+
+    def __str__(self):
+        return self.location_name
+
+    def save_location(self):
+        self.save  
+
+class Category(models.Model):
+    category_name=models.CharField(max_length=30,unique=True)
+
+    def __str__(self):
+        return self.category_name
+
+    def save_category(self):
+        self.save 
+    def delete_category(self):
+        self.delete     
